2daf284db881cd109aef3d63b2f4e37c1e6fbcbe
Felipe B. Castro <fbc26232@gmail.com>
2024-11-29 20:34:49 -0300

    Update TestConnection.java

--- src/TestConnection.java
+++ src/TestConnection.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class TestConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/ecommerce";
+        String user = "seu_usuario";
+        String password = "sua_senha";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Conexão bem-sucedida!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
